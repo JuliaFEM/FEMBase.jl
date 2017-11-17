e1a4ddfdad26ae0a72c13d78d9455dc0e544a215
Jukka Aho <ahojukka5@gmail.com>
2017-11-17 11:32:08 +0200

    field tests 100 %

--- test/test_fields.jl
+++ test/test_fields.jl
@@ -174,3 +174,29 @@ end
     f = Field(f1)
     @test isa(f, DVTI)
 end
+
+@testset "test interpolation of discrete constant time-variant field" begin
+    f = DCTV(0.0 => 0.0, 1.0 => 1.0)
+    @test isapprox(f(-1.0), DCTI(0.0))
+    @test isapprox(f( 0.0), DCTI(0.0))
+    @test isapprox(f( 0.3), DCTI(0.3))
+    @test isapprox(f( 0.5), DCTI(0.5))
+    @test isapprox(f( 0.9), DCTI(0.9))
+    @test isapprox(f( 1.0), DCTI(1.0))
+    @test isapprox(f( 1.5), DCTI(1.0))
+
+    f2 = DCTV(0.0 => 0.0, 0.25 => -0.1, 0.50 => -0.1)
+    @test isapprox(f2(0.0), DCTI(0.0))
+    @test isapprox(f2(0.25), DCTI(-0.1))
+    @test isapprox(f2(0.50), DCTI(-0.1))
+    @test isapprox(f2(0.35), DCTI(-0.1))
+end
+
+@testset "default fields" begin
+    @test DCTI().data == nothing
+    @test Field().data == nothing
+    @test DCTI(1) == DCTI(1)
+    @test DCTI(1) == 1
+    @test length(DCTI(1)) == 1
+    @test length(DVTV()) == 0
+end
